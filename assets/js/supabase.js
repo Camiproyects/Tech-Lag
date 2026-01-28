d990aa7c5451648c24942fe13dc4f6342537dbcb
Camiproyects <andresillopillo2004@gmail.com>
2026-01-27 22:15:59 -0500

    Actualizar supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -4,7 +4,7 @@
 
 const SUPABASE_URL = "https://nhiqjzjdztoyqsvnrkai.supabase.co";
 const SUPABASE_ANON_KEY =
-  "sb_publishable__pjEInzOogtdZ-m2INqFug_ZMulzlsm";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oaXFqempkenRveXFzdm5ya2FpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3MDEyMzcsImV4cCI6MjA4NDI3NzIzN30.SlFOQvYJAiOTZvX-Da8bUmASvZ8PSfUe30pQTip4bUc";
 
 /* Validación básica */
 if (typeof supabase === "undefined") {
