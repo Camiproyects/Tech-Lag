29b0dac27b59808e6259709c21f7639131e44514
Camiproyects <andresillopillo2004@gmail.com>
2026-01-27 23:07:07 -0500

    Actualizar supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -3,16 +3,15 @@
 /* ============================= */
 
 const SUPABASE_URL = "https://nhiqjzjdztoyqsvnrkai.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oaXFqempkenRveXFzdm5ya2FpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3MDEyMzcsImV4cCI6MjA4NDI3NzIzN30.SlFOQvYJAiOTZvX-Da8bUmASvZ8PSfUe30pQTip4bUc";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oaXFqempkenRveXFzdm5ya2FpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3MDEyMzcsImV4cCI6MjA4NDI3NzIzN30.SlFOQvYJAiOTZvX-Da8bUmASvZ8PSfUe30pQTip4bUc";
 
-/* Validación básica */
-if (typeof supabase === "undefined") {
+/* Validación del SDK */
+if (typeof window.supabase === "undefined") {
   throw new Error("Supabase SDK no cargado");
 }
 
 /* Cliente Supabase */
-const supabase = supabase.createClient(
+const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
